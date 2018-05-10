5f6026dfc64f41b78b16d5cb1abec852ec92f938
mdwareing <mdwareing@gmail.com>
2018-05-10 14:50:59 +0100

    Hidden keys in env file

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
+require('dotenv').config();
 var Twit = require('twit');
-
 var config = require('./config');
 var T = new Twit(config);
 
@@ -11,7 +11,7 @@ function tweetIt() {
   var r = Math.floor(Math.random()*100);
   
   var tweet = {
-    status: ` here is a random number ${r} #codingrainbow from node.js`
+    status: `Here is a random number ${r} #codingrainbow from node.js`
   }
 
   T.post('statuses/update', tweet, tweeted);
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  consumer_key:         'kpqd4CCGqwVGNliknLEvqY8qD',
-  consumer_secret:      'nEeXyW8bdubmJiUchiReukiwN85InQikUh9IKSpM2Nk4NNnfOE',
-  access_token:         '942436818909388801-3mmWL4qe17yUld3Xg58GGB3pJDBQRLC',
-  access_token_secret:  'mqdotitWpN7CJLv1ECNfYsHauoCBXBXoi2E2phCSeGTxD',
+  consumer_key:         process.env.CONSUMER_KEY,
+  consumer_secret:      process.env.SECRET_KEY,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
